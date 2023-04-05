771e6073035b2a3f387fd4278cfe9b0c84672aae
Evgeny Morash <evm037@mail.ru>
2023-04-05 23:28:00 +0300

    add: styles for loader

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import loader from './loader.gif'
+import './loader.css'
 
 export function Loader() {
     return (
-        <div>
+        <div className='loader'>
             <h2>Loading...</h2>
             <img src={loader} alt="Loader"/>
         </div>
