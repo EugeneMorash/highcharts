20dd876180d04236a86180c0774fb653a9b69657
Evgeny Morash <evm037@mail.ru>
2023-04-05 10:40:17 +0300

    add: highcharts with test data

--- src/components/Highcharts.tsx
+++ src/components/Highcharts.tsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from 'highcharts';
-import {render} from "react-dom";
+import {useAppSelector} from "../hook/hook";
+import {Button, ButtonGroup} from "@material-ui/core";
 
-const options = {
-    chart: {
-        type: 'spline'
-    },
-    title: {
-        text: 'My chart'
-    },
-    series: [
-        {
-            data: [1, 2, 1, 4, 3, 6]
-        }
-    ]
-};
 
 export function HighchartsGraph() {
+
+    const appData = useAppSelector((state) => state.appData)
+
+    const options = {
+        yAxis: {
+            title: {
+                text: 'ВДС НПЗ, руб/тонну'
+            }
+        },
+        xAxis: {
+            title: {
+                text: 'Переработанное нефтяное сырье накопленным итогом, тонн'
+            }
+        },
+        legend: {
+            align: 'right',
+            verticalAlign: 'top'
+        },
+        // chart: {
+        //     type: 'spline'
+        // },  //* плавный изгиб
+        title: {
+            text: ''
+        },
+        series: [
+            {
+                name: 'С учетом субсидии',
+                data: [1, 2, 1, 4, 3, 6]
+            },
+            {
+                name: 'Без учета субсидии',
+                data: [2, 3, 5, 4, 1, 3]
+            },
+
+        ]
+    };
+
     return (
         <div>
+            {/* eslint-disable-next-line react/jsx-no-undef */}
+            <ButtonGroup color="primary" aria-label="outlined primary button group">
+                <Button>2021</Button>
+                <Button>2022</Button>
+                <Button>2023</Button>
+                <Button>2024</Button>
+            </ButtonGroup>
+
             <HighchartsReact highcharts={Highcharts} options={options}/>
         </div>
     );
 }
-
-render(<HighchartsGraph/>, document.getElementById('root'));
